71d66d4ac413f0afe73f41d40403b4943e582469
Khanh Bui <kbuiblue@gmail.com>
2023-11-19 13:05:18 +0700

    feat: add error handling for failed API calls

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,20 +1,32 @@
-import { Flex, LoadingOverlay } from '@mantine/core';
+import { Flex, LoadingOverlay, Alert } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import Question from '../Question/Question';
 import CheckAnswersButton from '../Buttons/CheckAnswersButton';
 import { TotalScoreProvider } from '../TotalScoreContext';
 import { QuestionType } from '@/types';
+import styles from '../Quiz/Quiz.module.css';
 
 export default function Quiz() {
   const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [error, setError] = useState<Error | null>(null);
 
-  useEffect(() => {
-    async function getQuestions() {
+  const getQuestions = async () => {
+    try {
       const response = await fetch('https://opentdb.com/api.php?amount=5&type=multiple');
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch questions.');
+      }
+
       const data = await response.json();
       setQuestions(data.results);
+    } catch (error: unknown) {
+      console.error('Error:', error);
+      setError(error as Error);
     }
+  };
 
+  useEffect(() => {
     getQuestions();
   }, []);
 
@@ -22,6 +34,7 @@ export default function Quiz() {
 
   return (
     <Flex
+      className={styles.quiz}
       pos="relative"
       direction="column"
       justify="center"
@@ -37,6 +50,17 @@ export default function Quiz() {
         {questions?.map(
           (question, index) =>
             isQuestion(question) && <Question key={index} {...question} id={index} />
+        )}
+        {error && (
+          <Alert
+            className={styles.alert}
+            variant="light"
+            color="red"
+            withCloseButton
+            title={error.name}
+          >
+            `Failed to fetch questions. Please refresh the page and try again.`
+          </Alert>
         )}
         <LoadingOverlay
           pos={'absolute'}
