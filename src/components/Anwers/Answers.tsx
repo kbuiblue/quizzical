48e6dddc6c6a1ce627555a8aceba1ac035d8f9ea
Khanh Bui <kbuiblue@gmail.com>
2023-11-22 22:17:09 +0700

    feat: conditional rendering for answers

--- src/components/Anwers/Answers.tsx
+++ src/components/Anwers/Answers.tsx
@@ -3,14 +3,16 @@ import { selectedAnswers } from '../../pages/Quiz.page';
 import { decodeHTML } from 'entities';
 import { useState, useEffect } from 'react';
 import { Chip, Group } from '@mantine/core';
+import { checkSelectedAnswers } from '../Buttons/CheckAnswersButton';
 import styles from '../Anwers/Answers.module.css';
 
 export default function Answers({ id, correct_answer, incorrect_answers }: AnswersType) {
   const [allAnswers, setAllAnswers] = useState([correct_answer, ...incorrect_answers]);
+  const [isInitialRender, setIsInitialRender] = useState(true);
 
   const shuffleAnswers = (answers: string[]) => {
     let currentIndex: number = answers.length,
-      randomIndex;
+      randomIndex: number;
 
     while (currentIndex > 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
@@ -23,8 +25,11 @@ export default function Answers({ id, correct_answer, incorrect_answers }: Answe
   };
 
   useEffect(() => {
-    const shuffledAnswers = shuffleAnswers(allAnswers);
-    setAllAnswers(shuffledAnswers);
+    if (isInitialRender) {
+      const shuffledAnswers = shuffleAnswers(allAnswers);
+      setAllAnswers(shuffledAnswers);
+      setIsInitialRender(false);
+    }
   }, []);
 
   return (
@@ -37,7 +42,15 @@ export default function Answers({ id, correct_answer, incorrect_answers }: Answe
             <Chip
               key={index}
               value={decodedAnswer}
-              className={styles.answer}
+              className={`${styles.answer} ${checkSelectedAnswers.value ? styles.isChecked : ''} ${
+                index === allAnswers.indexOf(selectedAnswers.value.get(id) || '' ) && index === allAnswers.indexOf(correct_answer)
+                  ? styles.isSelected
+                  : ''
+              } ${
+                index === allAnswers.indexOf(correct_answer) && checkSelectedAnswers.value
+                  ? styles.isCorrect
+                  : styles.isIncorrect
+              }`}
               onClick={() => {
                 selectedAnswers.value.set(id, decodedAnswer);
               }}
